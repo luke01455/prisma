0584379b3bdc0cec6c53edbd1362524b72005b20
luke01455 <luke0145555@hotmail.co.uk>
2019-12-18 20:25:03 +0000

    refactored some redundant code and setup prisma exists for error handling

--- src/prisma.js
+++ src/prisma.js
@@ -8,8 +8,20 @@ const prisma = new Prisma({
 // prisma.query prisma.mutation prisma.subscription prisma.exists
 // method name matches with query name from schema
 
+// prisma.exists.Comment({
+//     id: "ck4bfosxn005507511l3aoun0",
+//     author: {
+//         id: "ck4bfexyu004m07517z0u3li0"
+//     }
+// }).then((exists) => console.log(exists))
 
 const createPostForUser = async (authorId, data) => {
+    const userExists = await prisma.exists.User({ id: authorId})
+
+    if(!userExists) {
+        throw new Error("user not found")
+    }
+
     const post = await prisma.mutation.createPost({
         data: {
             ...data,
@@ -19,43 +31,42 @@ const createPostForUser = async (authorId, data) => {
                 }
             }
         }
-    }, '{ id }')
-    const user = await prisma.query.user({
-        where: {
-            id: authorId
-        }
-    }, '{ id name email posts { id title published } }')
+    }, '{ author { id name email posts { id title published }} }')
 
-    return user 
+    return post.author 
 }
 
 // createPostForUser('ck4bd0h9v001n0751br0ul0sx', {
-//     title: 'great books to read',
-//     body: 'the art of war',
+//     title: 'new books to read',
+//     body: 'lord of the rings',
 //     published: true
 // }).then((user) => {
 //     console.log(JSON.stringify(user, undefined, 2))
+// }).catch((error) => {
+//     console.log(error)
 // })
 
 
 const updatePostForUser = async (postId, data) => {
+    const postExists = await prisma.exists.Post({ id: postId})
+
+    if(!postExists) {
+        throw new Error('post not found')
+    }
     const post = await prisma.mutation.updatePost({
         data,          
         where: {
             id: postId
         }
-    }, '{ author { id } }')
-    const user = await prisma.query.user({
-        where: {
-            id: post.author.id
-        }
-    }, '{ id name email posts { id title published } }')
+    }, '{ author { id name email posts { id title published body}} }')
 
-    return user 
+    return post.author 
 }
-// updatePostForUser('ck4bln5pc006p0751rzaue5q2', {
-//     title: 'great books',
-//     body: 'big lebowski'
-// }).then((user) => {
-//     console.log(JSON.stringify(user, undefined, 2))
-// })
+
+updatePostForUser('ck4bqnl5g00770751vsor8jxb', {
+    body: 'lord of the rings 2'
+}).then((user) => {
+    console.log(JSON.stringify(user, undefined, 2))
+}).catch((error) => {
+    console.log(error)
+})
