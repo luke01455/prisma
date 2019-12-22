605618760b87a56571ae5bd682fb5b624201ace5
luke01455 <luke0145555@hotmail.co.uk>
2019-12-22 22:38:08 +0000

    commit for heroku

--- src/prisma.js
+++ src/prisma.js
@@ -1,8 +1,9 @@
 import { Prisma } from 'prisma-binding'
 import { fragmentReplacements } from './resolvers/index'
+
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'http://192.168.99.100:4466/',
+    endpoint: process.env.PRISMA_ENDPOINT,
     secret: 'secretkey123',
     fragmentReplacements
 })
